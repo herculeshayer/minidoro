5239c319561b0e20f8db45d06365a2df5b5830bf
Harman Hayer <hhhayer@gmail.com>
2021-07-07 14:20:04 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('minidoro');
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+var app = express();
+//declare new express app
+app.use(express.json());
+
+//enable cors for all methods
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next();
+})
+
+
+
+
+app.listen(process.env.PORT, ()=>console.log(`listening on ${process.env.PORT}`))
