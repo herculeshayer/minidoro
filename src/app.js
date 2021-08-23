4050a1389d3d6a5117a3b6751bd563138fd51bd0
Harman Hayer <hhhayer@gmail.com>
2021-08-23 14:20:53 -0700

    auth0 login functionality

--- src/app.js
+++ src/app.js
@@ -3,8 +3,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: `${process.env.secret}`,
+    baseURL: 'http://localhost:3000',
+    clientID: 'a8hvNO1ECUwNxnLrWuASR8JUqvOZ9g95',
+    issuerBaseURL: 'https://dev-z69jo6so.us.auth0.com'
+  };
 
 var app = express();
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
 //declare new express app
 app.use(express.json());
 
@@ -16,8 +39,9 @@ app.use((req, res, next) => {
 })
 
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/home', express.static(path.join(__dirname, '/public')))
 
-app.use('/login', require('./routes/User'));
+// app.use('/callback', require('./route'))
+// app.use('/login', require('./routes/User'));
 
 app.listen(process.env.PORT, ()=>console.log(`listening on ${process.env.PORT}`))
