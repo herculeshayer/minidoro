c87466814c2434a0186d94118843dd73b74841fd
Harman Hayer <hhhayer@gmail.com>
2021-08-23 15:14:35 -0700

    btn now redirects to login

---
+++ src/public/scripts.js
@@ -0,0 +1,6 @@
+function changePage() {
+    // var linkBtn = document.getElementById('login-button');
+    // // linkBtn.style.getPropertyValue = "yo"
+    // // linkBtn
+    window.location = "/login";
+}
