202ff7969b2060647cbdb96a20d645b72d7ac98f
Harman Hayer <hhhayer@gmail.com>
2021-08-26 20:59:44 -0700

    scrapped auth0 approach for jwt postgreSQL

--- src/app.js
+++ src/app.js
@@ -1,61 +1,26 @@
 require('dotenv').config();
 const express = require('express');
-// const mongoose = require('mongoose');
+// const pg = require('pg');
 const path = require('path');
 
-const { auth } = require('express-openid-connect');
-const { requiresAuth } = require('express-openid-connect');
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    session: {
-      cookie: {
-        domain: 'https://minidoro.herokuapp.com/'
-      }
-    },
-    secret: `${process.env.secret}`,
-    baseURL: 'http://localhost:3000',
-    clientID: 'a8hvNO1ECUwNxnLrWuASR8JUqvOZ9g95',
-    issuerBaseURL: 'https://dev-z69jo6so.us.auth0.com'
-  };
-
 var app = express();
+
 //declare new express app
-// app.use(express.json());
+app.use(express.json());
 
-//enable cors for all methods
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header("Access-Control-Allow-Headers", "*")
-//     next();
-// })
-
-app.use(auth(config));
-
-// app.use('/login', require('./routes/Auth0'));
-
-app.get('/', (req, res) => {
-
-    if(req.oidc.isAuthenticated()) {
-      res.redirect('/profile')
-    }
-    // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-      
-});
+// enable cors for all methods
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next();
+})
 
 // app.use('/', require('./routes/db-connection.js'));
 
-// app.use('/profile', require('./routes/Profile'));
 
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
+app.use('/api', require('./routes/User'))
+app.use('/', express.static(path.join(__dirname, '/public')))
 
-// app.use('/', require('./routes/'))
-app.use('/home', express.static(path.join(__dirname, '/public')))
 
-// app.use('/callback', require('./route'))
-// app.use('/user', require('./routes/User'));
 
 app.listen(process.env.PORT, () => console.log(`listening on ${process.env.PORT}`))
