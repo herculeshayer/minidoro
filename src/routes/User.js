ff03c53bc5a3335f4a244ca92bfe85eef3d32217
Harman Hayer <hhhayer@gmail.com>
2021-09-07 13:40:34 -0700

    success: query DB for id

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,21 +1,60 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
-router.get('/user', (req, res) => {
+const db = require('../db');
+
+router.get('/user/:id', (req, res, next) => {
     try {
-        res.json({message: 'yolo'})
-        res.json(req.body);
+        db.query('SELECT * FROM users WHERE user_id = $1', [req.params.id], (err, result) => {
+            if(err) {
+                return next(err)
+            }
+            res.send(result.rows[0])
+        })
+        // res.json({message: 'yolo'});
+        // res.json(req.body);
     } catch (error) {
         
     }
 })
+//registration
+router.post('/user', async (req,res,next) => {
+    const { username, password, email } = req.body; // i guess we can alter the name of the req.body variable
 
-router.post('/user', (req,res) => {
+    // if(!email || typeof email !== 'string') {
+    //     return res.json({status: 'error'});
+    // }
+    // if(!plainTextPassword || typeof plainTextPassword < 5) {
+    //     return res.json({status: 'error'});
+    // }
+
     try {
-        res.json({message: "recieved"})
+        db.query(`INSERT INTO users (username, password, email) VALUES (${username}, ${password}, ${email});`, null, (err,result) => {
+                if(err) {
+                    return next(err)
+                }
+                res.send(result.rows[0]);
+            });
+        // console.log(await bcrypt.hash(plainTextPassword, 10));
+        res.json({message: "recieved"});
     } catch (error) {
-        res.json({message: error})
+        if(error.code === 11000) {
+            res.json(`window.alert('duplicate key')`);
+        }
+        throw error;
+        // res.json({message: error})
     }
 })
+//login
+// router.post('/user/login', (req, res) => {
+//     try {
+//         res.send({message: "user successfully logged in"})
+//     } catch (error) {
+        
+//     }
+// })
+
+
 
 module.exports = router;
