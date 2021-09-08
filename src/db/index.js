b48c75a8b71e5a5d1e1d1bb5eba1ab29344c19f4
Harman Hayer <hhhayer@gmail.com>
2021-09-08 13:58:51 -0700

    set ssl true, heroku test commit

--- src/db/index.js
+++ src/db/index.js
@@ -9,15 +9,19 @@ const { Pool, defaults } = require('pg');
 //     }
 // }
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // port: process.env.DB_PORT,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
-    ssl: false
+    ssl: true,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 pool.on('connect', ()=> console.log('Connection Successful'));
 // pool.on('error', ()=> console.log(error))
