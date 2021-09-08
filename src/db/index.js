88924ab3249a1e9effdd3226dce2715e6638c0ea
Harman Hayer <hhhayer@gmail.com>
2021-09-08 16:03:31 -0700

    add prod + dev connections

--- src/db/index.js
+++ src/db/index.js
@@ -3,35 +3,63 @@ require('dotenv').config();
 const { Pool, defaults } = require('pg');
 
 // console.log(`${process.env.HEROKU_DATABASE_URL}`)
-// if(process.env.NODE_ENV === 'production') {
-//     defaults.ssl = {
-//         rejectUnauthorized: false
-//     }
-// }
-const pool = new Pool({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // port: process.env.DB_PORT,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    connectionString: process.env.DATABASE_URL,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    ssl: true,
-    ssl: {
-        rejectUnauthorized: false
+if(process.env.NODE_ENV == 'development') {
+    const pool = new Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        // connectionString: process.env.DATABASE_URL,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+        ssl: false,
+        // ssl: {
+        //     rejectUnauthorized: false
+        // }
+    });
+    pool.on('connect', ()=> console.log('Connection Successful'));
+    // pool.on('error', ()=> console.log(error))
+    pool.on('error', err => console.log(err));
+
+    // pool.connect()
+
+    module.exports = {
+        query: (text, params, callback) => {
+            return pool.query(text, params, callback)
+        },
     }
-});
-pool.on('connect', ()=> console.log('Connection Successful'));
-// pool.on('error', ()=> console.log(error))
-pool.on('error', err => console.log(err));
+} 
+if(process.env.NODE_ENV == 'production') {
+    const pool = new Pool({
+        // host: process.env.DB_HOST,
+        // user: process.env.DB_USER,
+        // port: process.env.DB_PORT,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
+        connectionString: process.env.DATABASE_URL,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+        ssl: true,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 
-// pool.connect()
+    pool.on('connect', ()=> console.log('Connection Successful'));
+    // pool.on('error', ()=> console.log(error))
+    pool.on('error', err => console.log(err));
 
-module.exports = {
-    query: (text, params, callback) => {
-        return pool.query(text, params, callback)
-    },
+    // pool.connect()
+
+    module.exports = {
+        query: (text, params, callback) => {
+            return pool.query(text, params, callback)
+        },
+    }
+
 }
+
 
