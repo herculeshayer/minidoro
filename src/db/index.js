df6a46b69b649d4028e77f1255f12fe30fd4d250
Harman Hayer <hhhayer@gmail.com>
2021-09-09 12:57:43 -0700

    remove commented code

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const { Pool, defaults } = require('pg');
 
-// console.log(`${process.env.HEROKU_DATABASE_URL}`)
 if(process.env.NODE_ENV == 'development') {
     const pool = new Pool({
         host: process.env.DB_HOST,
@@ -10,20 +9,13 @@ if(process.env.NODE_ENV == 'development') {
         port: process.env.DB_PORT,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        // connectionString: process.env.DATABASE_URL,
         max: 20,
         idleTimeoutMillis: 30000,
         connectionTimeoutMillis: 2000,
         ssl: false,
-        // ssl: {
-        //     rejectUnauthorized: false
-        // }
     });
     pool.on('connect', ()=> console.log('Connection Successful'));
-    // pool.on('error', ()=> console.log(error))
     pool.on('error', err => console.log(err));
-
-    // pool.connect()
 
     module.exports = {
         query: (text, params, callback) => {
@@ -33,11 +25,6 @@ if(process.env.NODE_ENV == 'development') {
 } 
 if(process.env.NODE_ENV == 'production') {
     const pool = new Pool({
-        // host: process.env.DB_HOST,
-        // user: process.env.DB_USER,
-        // port: process.env.DB_PORT,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
         connectionString: process.env.DATABASE_URL,
         max: 20,
         idleTimeoutMillis: 30000,
@@ -49,10 +36,10 @@ if(process.env.NODE_ENV == 'production') {
     });
 
     pool.on('connect', ()=> console.log('Connection Successful'));
-    // pool.on('error', ()=> console.log(error))
+   
     pool.on('error', err => console.log(err));
 
-    // pool.connect()
+
 
     module.exports = {
         query: (text, params, callback) => {
