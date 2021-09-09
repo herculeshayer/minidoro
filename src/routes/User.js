550fe1cc2bd780a3fb1a53ba8746ed2435890cb1
Harman Hayer <hhhayer@gmail.com>
2021-09-09 15:53:40 -0700

    RegisterPOST-OK, GETbyID-OK, LoginPOST-NO

--- src/routes/User.js
+++ src/routes/User.js
@@ -4,45 +4,26 @@ const router = express.Router();
 
 const db = require('../db');
 
-router.get('/user', (req,res) => {
-    try {
-        res.json({message: 'user'})
-    } catch (error) {
-        res.json({message: error})
-    }
-})
-
-router.get('/user/:id', (req, res, next) => {
-    try {
-        db.query('SELECT * FROM users WHERE user_id = $1', [req.params.id], (err, result) => {
-            if(err) {
-                return next(err)
-            }
-            res.send(result.rows[0])
-        })
-        // res.json({message: 'yolo'});
-        // res.json(req.body);
-    } catch (error) {
-        
-    }
-})
 //registration
-router.post('/user', async (req,res,next) => {
-    const { username, password, email } = req.body; // i guess we can alter the name of the req.body variable
+router.post('/register', async (req,res,next) => {
+    const { username, password: plainTextPassword, email } = req.body; // i guess we can alter the name of the req.body variable
 
-    // if(!email || typeof email !== 'string') {
-    //     return res.json({status: 'error'});
-    // }
-    // if(!plainTextPassword || typeof plainTextPassword < 5) {
-    //     return res.json({status: 'error'});
-    // }
+    if(!email || typeof email !== 'string') {
+        return res.json({status: 'error'});
+    }
+    if(!plainTextPassword || typeof plainTextPassword < 5) {
+        return res.json({status: 'error'});
+    }
 
     try {//need to check out correct params for query
+        const password = await bcrypt.hash(plainTextPassword, 10);
         db.query(`INSERT INTO users(username, password, email) VALUES ($1,$2,$3);`, [username,password,email], (err,result) => {
                 if(err) {
                     return next(err)
                 }
-                res.status(201).send(`User: ${username} w/ email: ${email} added`);
+                // res.redirect(201, '../public/login.html');
+                //, "data": `User: ${username} w/ email: ${email} added`
+                res.json({ status: 'OK'});
             });
         // console.log(await bcrypt.hash(plainTextPassword, 10));
         // res.json({message: "recieved"});
@@ -56,8 +37,40 @@ router.post('/user', async (req,res,next) => {
 })
 
 //login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    
+    // const hashedPassword = await bcrypt.compare();
 
+    try {
+        const dbPassword = db.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
+            if(err) {
+                return next(err)
+            }
+            return result.row[0];
+        })
+        const hashedPassword = await bcrypt.compare(password, dbPassword.password);
+        console.log(hashedPassword);
+    } catch (error) {
+        throw error;
+    }
+    res.json({status: 'ok'})
+})
 
-
+//login
+router.get('/login/:id', (req, res, next) => {
+    try {
+        db.query('SELECT * FROM users WHERE username = $1', [req.params.id], (err, result) => {
+            if(err) {
+                return next(err)
+            }
+            res.send(result.rows[0])
+        })
+        // res.json({message: 'yolo'});
+        // res.json(req.body);
+    } catch (error) {
+        
+    }
+})
 
 module.exports = router;
