12b7d86d6e0ad0f311a46331a80db0193b9f006f
Harman Hayer <hhhayer@gmail.com>
2021-12-07 19:20:27 -0800

    attempt to create userdata endpoint for access

---
+++ src/routes/userData.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Router = require('express-promise-router');
+const bcrypt = require('bcryptjs');
+const db = require('../db');
+const jwt = require('jsonwebtoken');
+
+const router = new Router();
+
+const { checkHeader } = require('./../components/checkHeader');
+
+router.get('/:id', checkHeader, async (req, res) => {
+    try {
+        res.send('hi?')
+    } catch (error) {
+        throw error;
+    }
+})
