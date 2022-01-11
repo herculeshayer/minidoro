8406ce81d1ea1d458e57a06cc9f5a2f08ac46f76
Harman <hhhayer@gmail.com>
2022-01-10 23:17:03 -0800

    fix: dev connection causing typeerror

--- src/db/index.js
+++ src/db/index.js
@@ -6,28 +6,10 @@ const { Pool, defaults } = require('pg');
     and a local dev DB... but it works and im going to use it 
 */
 
-if(process.env.NODE_ENV == 'development') {
-    const pool = new Pool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        port: process.env.DB_PORT,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        max: 20,
-        idleTimeoutMillis: 30000,
-        connectionTimeoutMillis: 2000,
-        ssl: false,
-    });
-    pool.on('connect', ()=> console.log('Connection Successful'));
-    pool.on('error', err => console.log(err));
-
-    module.exports = {
-        query: (text, params, callback) => {
-            return pool.query(text, params, callback)
-        },
-        asyncQuery: (text, params) => pool.query(text, params),
-    }
-} 
+// if(process.env.NODE_ENV == 'development') {
+//     console.log('from development db');
+    
+// }
 if(process.env.NODE_ENV == 'production') {
     const pool = new Pool({
         connectionString: process.env.DATABASE_URL,
@@ -53,6 +35,27 @@ if(process.env.NODE_ENV == 'production') {
         asyncQuery: (text, params) => pool.query(text, params),
     }
 
+} else {
+    const pool = new Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+        ssl: false,
+    });
+    pool.on('connect', ()=> console.log('Connection Successful'));
+    pool.on('error', err => console.log(err));
+
+    module.exports = {
+        query: (text, params, callback) => {
+            return pool.query(text, params, callback)
+        },
+        asyncQuery: (text, params) => pool.query(text, params),
+    }
 }
 
 
