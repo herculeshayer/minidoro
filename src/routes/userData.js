c99e74e935663d40c7b4b9834ec5ead5ec8aa9f1
Harman <hhhayer@gmail.com>
2022-01-24 12:33:13 -0800

    test route for cookies

--- src/routes/userData.js
+++ src/routes/userData.js
@@ -5,12 +5,11 @@ const db = require('../db');
 const jwt = require('jsonwebtoken');
 
 const router = new Router();
-
 const { checkHeader } = require('./../components/checkHeader');
 
 router.get('/:id', checkHeader, async (req, res) => {
     try {
-        res.send('hi?')
+        res.send('user info');
     } catch (error) {
         throw error;
     }
