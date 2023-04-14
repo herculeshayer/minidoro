2dd85b5416843c36b96dae1bcc263363fbdcd848
harman <hhhayer@gmail.com>
2023-04-13 22:09:33 -0700

    add application underdevelopment tag

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,9 +4,10 @@ import tomatoImg from './../assets/tomato-icon.svg'
 const About = () => {
     return (
         <>
+            <p style={{fontSize: "20px", paddingBottom: "2rem"}}>Application underdevelopment</p>
             <img src={tomatoImg} alt="tomato" style={{height: "35%", paddingBottom: "5%"}}/>
 
-            <p>Welcome - this is a pomodoro timer application </p>
+            <p>Welcome - this is a pomodoro timer application</p>
             <br/>
             <p>Tools used in the development of this application:
                 <div style={{fontWeight: "550"}}>
