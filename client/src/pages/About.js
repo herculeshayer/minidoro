b24195a391577dfe8f93aa71c5e58ce83d5c08e8
harman <hhhayer@gmail.com>
2023-04-13 21:52:45 -0700

    add image of tomato and detail of project type

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import tomatoImg from './../assets/tomato-icon.svg'
 const About = () => {
     return (
         <>
-            This is a pomodoro timer application<br/>
-            For more information about Pomodoro Technique<br/>
+            <img src={tomatoImg} alt="tomato" style={{height: "35%", paddingBottom: "5%"}}/>
+
+            <p>Welcome - this is a pomodoro timer application </p>
+            <br/>
+            <p>Tools used in the development of this application:
+                <div style={{fontWeight: "550"}}>
+                    React.js, Node.js, Express, PostgreSQL, SCSS - Hosting: [AWS, Netlify, Render]
+                </div>
+            </p>
+            <br/>
+            {/*<p>If already a user, login. Otherwise, <a href="">register.</a></p>*/}
+            <p>For more information about the Pomodoro Technique<br/></p>
+
             Visit: <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique">Wikipedia: Pomodoro Technique</a>
         </>
     );
