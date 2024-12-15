8844a1912271c6370ec02ad4dadae7f5ec861487
harman <hhhayer@gmail.com>
2024-12-14 22:51:39 -0800

    Db setup script (#37)
    * add postgresql dockerfile
    
    * add env.example file w/ template values
    
    * add docker image with default db env variables
    
    * .env template to follow for future git clones
    
    * unnecessary heroku junk
    
    * feature: split db table setup script to setup.js
    
    * spelling mistake
    
    * add test user to db on init

--- server/src/db/setup.js
+++ server/src/db/setup.js
@@ -1,28 +1,70 @@
-// const db = require("./index");
+require("dotenv").config();
 
-/**
- * TODO:
- * 
- * This file should be called a single time up on build
- * It will drop any tables called users and then create 
- * a table called users -- atm a table is created inside db/index.js
- */
+const { Client } = require("pg");
 
-// const setup = () => {
-//     console.log('setup file');
-//     db.asyncQuery(`CREATE TABLE IF NOT EXISTS users (
-//         user_id serial PRIMARY KEY,
-//         username VARCHAR(50) UNIQUE NOT NULL,
-//         password VARCHAR(50) NOT NULL,
-//         email VARCHAR(255) UNIQUE NOT NULL,
-//         created_at TIMESTAMP DEFAULT
-//     )`,[], (err, result) => {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log(result);
-//         }
-//     });
-// }
+async function createTestUser(client) {
+  const result = await client.query(
+    `insert into users (username, password, email) values ('test', '123123', 'test@test.ca');`
+  );
+  console.log("createTestUser", result);
+}
 
-// module.exports = setup;
+async function createUserTable(client) {
+  await client.connect(() => console.log("Connection Successful"));
+
+  const result = await client.query(
+    `create table if not exists users(
+    userid uuid default gen_random_uuid(),
+    username varchar(50) unique not null,
+    password varchar(255) not null,
+    email varchar(255) unique not null,
+    created_at timestamp default current_timestamp,
+    primary key (userid)
+  );`
+  );
+  console.log("createUserTable", result);
+
+  await client.end();
+  console.log("Closing Connection");
+}
+
+async function setup() {
+  console.log("Running PSQL Setup Script");
+
+  if (process.env.NODE_ENV == "production") {
+    console.log("Environment set to Production");
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_URI,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+      ssl: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    createUserTable(client);
+    createTestUser(client);
+  }
+
+  if (process.env.NODE_ENV == "development") {
+    console.log("Environment set to Development");
+
+    const client = new Client({
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DB,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+      ssl: false,
+    });
+    createUserTable(client);
+    createTestUser(client);
+  }
+}
+
+setup();
