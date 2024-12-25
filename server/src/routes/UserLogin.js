49d0339054c689d12fad9f83827bb5922c83a84d
harman <hhhayer@gmail.com>
2024-12-25 12:18:14 -0800

    prettier went ham and removed commented nonsense

--- server/src/routes/UserLogin.js
+++ server/src/routes/UserLogin.js
@@ -1,94 +1,73 @@
-require('dotenv').config();
-const Router = require('express-promise-router');
-const bcrypt = require('bcryptjs');
-const db = require('../db');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const Router = require("express-promise-router");
+const bcrypt = require("bcryptjs");
+const db = require("../db");
+const jwt = require("jsonwebtoken");
 
 const router = new Router();
 
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
 
-// router.get('/:id', async (req, res) => {
-//     try {
-//         const rows = await db.asyncQuery('SELECT * FROM users WHERE username = $1', [req.params.id]);
+  try {
+    const databaseUser = await db.asyncQuery(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    const databaseUserName = databaseUser.rows[0].username;
+    const databasePassword = databaseUser.rows[0].password;
 
-//         console.log('cookies', req.cookies);
-//         console.log('user', rows.rows[0]);
-//         res.send(rows.rows[0]);
-//     } catch (error) {
-//         throw error;
-//     }
-// })
-
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-
-
-    try {
-        const databaseUser = await db.asyncQuery('SELECT * FROM users WHERE username = $1', [username])
-        const databaseUserName = databaseUser.rows[0].username;
-        const databasePassword = databaseUser.rows[0].password;
-
-        /**
-         * Maybe give below a try if everything else doesn't work
-         */
-        // if (req.method === "OPTIONS") {
-        //     res.header('Access-Control-Allow-Origin', req.headers.origin);
-        // } else {
-        //     res.header('Access-Control-Allow-Origin', '*');
-        // }
-
-        /**
-         * If no password or username is entered, send json response
-         */
-        if(!databasePassword | !databaseUserName) {
-            res.json({ status: "error", error: 'invalid username/password'})
-        } 
-        //We're passing back success JSON
-        //If password in db matches req.body password, execute conditional
-        if(await bcrypt.compare(password,databasePassword)) {
-            console.log(await bcrypt.compare(password,databasePassword))
-            // res.send(databaseUser);
-            
-            //This info is not hidden, don't pass sensitive info
-            //User token
-            jwt.sign({
-                    id: databaseUser.rows[0].user_id,
-                    username: databaseUserName,
-                    email: databaseUser.rows[0].email
-                }, 
-                process.env.JWT_SECRET, {
-                    expiresIn: '1h'
-                }, (err, token) => {
-                    if (err) {
-                        console.log(err);
-                    } 
-
-                    if(token) {
-                        res.cookie('access-token', token, {
-                            maxAge: 3600000,
-                            httpOnly: true,
-                            sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
-                            secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
-                            // domain: process.env.COOKIE_DOMAIN
-                        })
-                        
-                    }
-                    // res.setHeader('Set-Cookie', token);
-
-                    // res.redirect('http://localhost:3000/dashboard');
-                    res.status(200).json({ status: 'OK', tokenData: token })
-                }
-            )
-
-        } else {
-
-            res.send('password does not match db password');
-        }
-
-    } catch (error) {
-        throw error;
+    /**
+     * If no password or username is entered, send json response
+     */
+    if (!databasePassword | !databaseUserName) {
+      res.json({ status: "error", error: "invalid username/password" });
     }
 
-})
+    //We're passing back success JSON
+    //If password in db matches req.body password, execute conditional
+    if (await bcrypt.compare(password, databasePassword)) {
+      console.log(await bcrypt.compare(password, databasePassword));
+      // res.send(databaseUser);
+
+      //This info is not hidden, don't pass sensitive info
+      //User token
+      jwt.sign(
+        {
+          id: databaseUser.rows[0].user_id,
+          username: databaseUserName,
+          email: databaseUser.rows[0].email,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1h",
+        },
+        (err, token) => {
+          if (err) {
+            console.log(err);
+          }
+
+          if (token) {
+            res.cookie("access-token", token, {
+              maxAge: 3600000,
+              httpOnly: true,
+              sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // must be 'none' to enable cross-site delivery
+              secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+              // domain: process.env.COOKIE_DOMAIN
+            });
+          }
+          // res.setHeader('Set-Cookie', token);
+
+          // res.redirect('http://localhost:3000/dashboard');
+          res.status(200).json({ status: "OK", tokenData: token });
+        }
+      );
+    } else {
+      res.send("password does not match db password");
+    }
+  } catch (error) {
+    throw error;
+  }
+});
 
 module.exports = router;
--- server/src/routes/UserRegistration.js
+++ server/src/routes/UserRegistration.js
@@ -1,47 +1,39 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
-// const Promise = require('express-promise-router');
-// const router2 = new Promise();
 
-
-const db = require('../db');
-// const { Router } = require('express');
+const db = require("../db/index");
 
 //register-user
-router.post('/', async (req, res, next) => {
-    const { username, password: plainTextPassword, email } = req.body; // i guess we can alter the name of the req.body variable
+router.post("/", async (req, res, next) => {
+  const { username, password: plainTextPassword, email } = req.body; // i guess we can alter the name of the req.body variable
 
-    if(!email || typeof email !== 'string') {
-        return res.json({status: 'error'});
+  if (!email || typeof email !== "string") {
+    return res.json({ status: "error" });
+  }
+  if (!plainTextPassword || typeof plainTextPassword < 5) {
+    return res.json({ status: "error" });
+  }
+
+  try {
+    const password = await bcrypt.hash(plainTextPassword, 10);
+    db.asyncQuery(
+      `INSERT INTO users(username, password, email) VALUES ($1,$2,$3);`,
+      [username, password, email]
+    );
+
+    res.json({ message: "recieved" });
+  } catch (error) {
+    if (error.code === 11000) {
+      console.log("hit");
+      res.json({ message: `window.alert('duplicate key')` });
     }
-    if(!plainTextPassword || typeof plainTextPassword < 5) {
-        return res.json({status: 'error'});
-    }
-
-    try {
-        const password = await bcrypt.hash(plainTextPassword, 10);
-        db.query(`INSERT INTO users(username, password, email) VALUES ($1,$2,$3);`, [username,password,email], (err,result) => {
-                if(err) {
-                    console.log(err);
-                    return next(err)
-                }
-                
-                res.json({ status: 'OK'});
-            });
-        // console.log(await bcrypt.hash(plainTextPassword, 10));
-        // res.json({message: "recieved"});
-    } catch (error) {
-        if(error.code === 11000) {
-            console.log('hit')
-            res.json({message: `window.alert('duplicate key')`});
-        }
-        // throw error;
-        console.log(error);
-        res.status(500).json({message: "error with registration, please try again"})
-    }
-})
-
-
+    // throw error;
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "error with registration, please try again" });
+  }
+});
 
 module.exports = router;
