80f84025faf63915631ee01064c33a0a07dcec68
harman <hhhayer@gmail.com>
2024-12-25 13:15:11 -0800

    Fix frontend api calls (#39)
    * delete pnpm usage, sticking with npm only
    
    * add example .env file for reference
    
    * simplified prod vs dev database setup with ternary operator
    
    * prettier went ham and removed commented nonsense
    
    * change node env to dev w/ nodemon setup
    
    * ts version change

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,47 +2,70 @@ require("dotenv").config();
 
 const { Pool, defaults } = require("pg");
 
-if (process.env.NODE_ENV == "production") {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URI,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    ssl: true,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+// if (process.env.NODE_ENV == "production") {
+//   const pool = new Pool({
+//     connectionString: process.env.DATABASE_URI,
+//     max: 20,
+//     idleTimeoutMillis: 30000,
+//     connectionTimeoutMillis: 2000,
+//     ssl: true,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
 
-  pool.on("connect", () => console.log("Connection Successful"));
-  pool.on("error", (err) => console.log(err));
+//   pool.on("connect", () => console.log("Connection Successful"));
+//   pool.on("error", (err) => console.log(err));
 
-  module.exports = {
-    query: (text, params, callback) => {
-      return pool.query(text, params, callback);
-    },
-    asyncQuery: (text, params) => pool.query(text, params),
-  };
-}
-if (process.env.NODE_ENV == "development") {
-  const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    port: process.env.POSTGRES_PORT,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    ssl: false,
-  });
-  pool.on("connect", () => console.log("Connection Successful"));
-  pool.on("error", (err) => console.log(err));
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback);
+//   },
+//   asyncQuery: (text, params) => pool.query(text, params),
+// };
+// }
+// if (process.env.NODE_ENV == "development") {
 
-  module.exports = {
-    query: (text, params, callback) => {
-      return pool.query(text, params, callback);
-    },
-    asyncQuery: (text, params) => pool.query(text, params),
-  };
-}
+const prodDB = {
+  connectionString: process.env.DATABASE_URI,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const devDB = {
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  port: process.env.POSTGRES_PORT,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  ssl: false,
+};
+
+const pool = new Pool(process.env.NODE_ENV == "production" ? prodDB : devDB);
+
+pool.on("connect", () => console.log("Connection Successful"));
+
+pool.on("error", (err) => console.log(err));
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+  asyncQuery: (text, params) => pool.query(text, params),
+};
+
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback);
+//   },
+//   asyncQuery: (text, params) => pool.query(text, params),
+// };
+// }
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,13 @@
-const usersRegistration = require('./UserRegistration')
-const userLogin = require('./UserLogin')
-const resetPassword = require('./ResetPassword');
-const userDashboard = require('./userDashboard');
-const testCookies = require('./testCookies');
+const usersRegistration = require("./UserRegistration");
+const userLogin = require("./UserLogin");
+const resetPassword = require("./ResetPassword");
+const userDashboard = require("./userDashboard");
+const testCookies = require("./testCookies");
 
-module.exports = app => {
-  app.use('/api/register', usersRegistration)
-  app.use('/api/login', userLogin)
-  app.use('/api/reset-password', resetPassword)
-  app.use('/api/dashboard', userDashboard)
-  app.use('/api/testcookie', testCookies)
-}
+module.exports = (app) => {
+  app.use("/api/register", usersRegistration);
+  app.use("/api/login", userLogin);
+  app.use("/api/reset-password", resetPassword);
+  app.use("/api/dashboard", userDashboard);
+  app.use("/api/testcookie", testCookies);
+};
