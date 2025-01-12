e7c952214a241648cb6f719c85aafb14bd9f6359
harman <hhhayer@gmail.com>
2025-01-11 21:04:09 -0800

    remove register from navbar, access /register from /login

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,5 @@
-
 import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom";
 import React, { useState } from "react";
-
-
 
 import Register from "./Register";
 import Login from "./Login";
@@ -10,38 +7,42 @@ import About from "./About";
 import Dashboard from "./Dashboard";
 
 const HomePage = () => {
-    /**
-     * Experiment with react router to get a feel for it
-     * 
-     * To get feel---
-     * 
-     * *****************EXPERIMENT SUCCESSFUL!!
-     */
+  /**
+   * Experiment with react router to get a feel for it
+   *
+   * To get feel---
+   *
+   * *****************EXPERIMENT SUCCESSFUL!!
+   */
 
-    return(
-        <section className="homepage-wrapper">
-            <BrowserRouter>
-            
-                <nav className="nav-wrapper">
-                    {/* <Link to="/">Home</Link> */}
-                    <Link className="navbar-item" to="/register">Register</Link>
-                    <Link className="navbar-item" to="/login">Login</Link>
-                    <Link className="navbar-item" to="/dashboard">Dashboard</Link>
-                    <Link className="navbar-item" to="/about">About</Link>
-                </nav>
-                <h1>MiniDoro</h1>
-            
-                <Routes>
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/" element={<Navigate to="/login" />} />
-                </Routes>
+  return (
+    <section className="homepage-wrapper">
+      <BrowserRouter>
+        <nav className="nav-wrapper">
+          {/* <Link to="/">Home</Link> */}
+          {/* <Link className="navbar-item" to="/register">Register</Link> */}
+          <Link className="navbar-item" to="/login">
+            Login
+          </Link>
+          <Link className="navbar-item" to="/dashboard">
+            Dashboard
+          </Link>
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+        </nav>
+        <h1>MiniDoro</h1>
 
-            </BrowserRouter>
-        </section>
-    );
-}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
+    </section>
+  );
+};
 
 export default HomePage;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,55 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { TiArrowRightThick } from 'react-icons/ti';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { TiArrowRightThick } from "react-icons/ti";
 
-import { postLoginInformation, getRedirectUser } from '../components/requestsAPI';
+import {
+  postLoginInformation,
+  getRedirectUser,
+} from "../components/requestsAPI";
 
 const Login = () => {
+  const [username, setUsername] = useState([]);
+  const [password, setPassword] = useState([]);
 
-    const [username, setUsername] = useState([]);
-    const [password, setPassword] = useState([]);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
+  const userRedirect = getRedirectUser(process.env.REACT_APP_REDIRECT_USER);
 
-    const userRedirect = getRedirectUser(process.env.REACT_APP_REDIRECT_USER);
-
-    /**
-     * Redirect if there is cookie present 
-     */
-    useEffect(() => {
-        if(userRedirect === true) {
-            alert("You've already logged in");
-            navigate('/dashboard')  
-        }
-    }, [userRedirect])
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        postLoginInformation(process.env.REACT_APP_LOGIN_API_URL, {
-            username, password
-        });
-
-        alert("Login Successful!");
-        navigate('/dashboard');
+  /**
+   * Redirect if there is cookie present
+   */
+  useEffect(() => {
+    if (userRedirect === true) {
+      alert("You've already logged in");
+      navigate("/dashboard");
     }
-   
-    return (
-        <section className="register-login">
-            <form onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input type="text" placeholder="Username" name="username" onChange={e=>setUsername(e.target.value)} />
-                <label>Password</label>
-                <input type="password" placeholder="Password" name="password" onChange={e=>setPassword(e.target.value)} />
-                <div>
-                    <button type="submit" style={{fontSize: 25}}><TiArrowRightThick /></button>
-                </div>
-            </form>
-        </section>
-        
+  }, [userRedirect]);
 
-    );
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    postLoginInformation(process.env.REACT_APP_LOGIN_API_URL, {
+      username,
+      password,
+    });
+
+    alert("Login Successful!");
+    navigate("/dashboard");
+  };
+
+  return (
+    <section className="register-login">
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div>
+          <button type="submit" style={{ fontSize: 25 }}>
+            <TiArrowRightThick />
+          </button>
+        </div>
+      </form>
+      <Link style={{ fontSize: "12px" }} to="/register">
+        Not Registered? Sign Up.
+      </Link>
+    </section>
+  );
+};
 
 export default Login;
