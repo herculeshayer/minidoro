e5f2503e5f1141f4e6714d9e1b537cee858b3014
harman <hhhayer@gmail.com>
2025-07-12 13:01:51 -0700

    button click fetches daily pomodoro complete

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -8,56 +8,50 @@ export default function Stats() {
    */
   const [pomodoroCount, setPomodoroCount] = useState(0);
 
-  useEffect(() => {
+  const handleSubmitDay = async (typeOfQuery) => {
+    // event.preventDefault();
+
     const controller = new AbortController();
     const signal = controller.signal;
-    const fetchData = async () => {
-      const date = new Date();
-      const response = await fetch(
-        `${import.meta.env.VITE_USER_POMODORO_DATA}/day`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "date-iso": date,
-          },
-          mode: "cors",
-          credentials: "include",
-          signal,
-        }
-      );
-
-      if (!response.ok) {
-        console.log(
-          `${import.meta.env.VITE_USER_POMODORO_DATA}/day: response not good: `,
-          response
-        );
-      } else {
-        const data = await response.json();
-        setPomodoroCount(data.completedPomodoros[0].pomodorocount);
-        console.log("data: ", data);
-        console.log(
-          "data.pomodorocountarray: ",
-          data.completedPomodoros[0].pomodorocount
-        );
+    const date = new Date();
+    const response = await fetch(
+      `${import.meta.env.VITE_USER_POMODORO_DATA}/day`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "date-iso": date,
+        },
+        mode: "cors",
+        credentials: "include",
+        signal,
       }
-    };
-    fetchData();
+    );
 
-    return () => {
-      controller.abort();
-    };
-  }, []);
+    if (!response.ok) {
+      console.log(
+        `${import.meta.env.VITE_USER_POMODORO_DATA}/day: response not good: `,
+        response
+      );
+    } else {
+      const data = await response.json();
+      setPomodoroCount(data.completedPomodoros[0].pomodorocount);
+      console.log("data: ", data);
+      console.log(
+        "data.pomodorocountarray: ",
+        data.completedPomodoros[0].pomodorocount
+      );
+    }
 
-  const handleSubmitDay = (event, typeOfQuery) => {
-    // event.preventDefault();
-    console.log(typeOfQuery);
+    console.log("TypeOfQuery: ", typeOfQuery);
   };
   return (
     <section>
       <h1>Stats Page</h1>
       <h1>{pomodoroCount}</h1>
-      <button onClick={() => handleSubmitDay("day")}>Day</button>
+      <button type="button" onClick={() => handleSubmitDay("day")}>
+        Day
+      </button>
     </section>
   );
 }
