d8da859639bbf26917efc13cf5a6987ec1436be1
harman <hhhayer@gmail.com>
2025-10-06 22:38:50 -0700

    display completed pomodoro data to user

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -7,18 +7,12 @@ import {
   YAxis,
   Tooltip,
 } from "recharts";
+
 export default function Stats() {
-  /**
-   * Make sure I'm not making several api calls on button clicks
-   * Need to check if the data is similar or something
-   * so each click does not overload the server with api calls
-   */
   const [pomodoroCount, setPomodoroCount] = useState([{}]);
   const [rangeOfPomodoroData, setRangeOfPomodoroData] = useState("");
 
   const handleSubmitDay = async (typeOfQuery) => {
-    // event.preventDefault();
-
     setRangeOfPomodoroData(typeOfQuery);
     const controller = new AbortController();
     const signal = controller.signal;
@@ -38,7 +32,7 @@ export default function Stats() {
     );
 
     if (!response.ok) {
-      console.log(
+      console.warn(
         `${
           import.meta.env.VITE_USER_POMODORO_DATA
         }/${typeOfQuery}: response not good: `,
@@ -46,57 +40,37 @@ export default function Stats() {
       );
     } else {
       const data = await response.json();
-      console.log("data.sessions.length: ", data.sessions.length);
-      setPomodoroCount(data.sessions);
-      // const pomoChart = () => {
-      //   <LineChart width={600} height={300} data={data.sessions}>
-      //     <CartesianGrid />
-      //     <Line dataKey="sessions" />
-      //     <XAxis dataKey="pomodoros" />
-      //     <YAxis />
-      //   </LineChart>;
-      // };
-      // if (data.sessions.length > 1) {
-      //   console.log("hit");
-      //   setPomodoroCount(data.sessions);
-      // } else {
-      //   setPomodoroCount(data.sessions[0].pomodorocount);
-      // }
-      // setPomodoroCount(data.sessions[0].pomodorocount);
-      console.log("data: ", data);
-      console.log(
-        "data.sessions[0].pomodorocount: ",
-        data.sessions[0].pomodorocount
-      );
-      console.log("data.sessions", data.sessions);
-    }
 
-    console.log("TypeOfQuery: ", typeOfQuery);
+      setPomodoroCount(data.sessions);
+    }
   };
 
-  /**
-   * need to display chart based on selection
-   */
+  const formatDate = (isoString) => {
+    const date = new Date(isoString);
+    return date.toLocaleDateString("en-US", { month: "short", day: "numeric" });
+  };
+
   return (
-    <section>
-      <h1>Stats Page</h1>
+    <section className="statspage-graph">
       <h1>{rangeOfPomodoroData}</h1>
-      {/* <h1>Pomodoro Count: {pomodoroCount}</h1> */}
-      <h1>Chart Below</h1>
-      <LineChart width={600} height={300} data={pomodoroCount}>
-        <CartesianGrid />
-        <Tooltip />
-        <Line dataKey="pomodorocount" type="monotone" activeDot={{ r: 4 }} />
-        <XAxis dataKey="sessiondate" />
-        <YAxis dataKey="pomodorocount" />
-      </LineChart>
-      <button
+      {rangeOfPomodoroData == "" ? (
+        ""
+      ) : (
+        <LineChart width={1000} height={400} data={pomodoroCount}>
+          <CartesianGrid />
+          <Tooltip labelFormatter={formatDate} />
+          <Line dataKey="pomodorocount" type="monotone" activeDot={{ r: 4 }} />
+          <XAxis dataKey="sessiondate" tickFormatter={formatDate} />
+          <YAxis dataKey="pomodorocount" />
+        </LineChart>
+      )}
+      {/* <button
         disabled={rangeOfPomodoroData === "Day"}
         type="button"
         onClick={() => handleSubmitDay("Day")}
       >
         Day
-      </button>
+      </button> */}
       <button
         disabled={rangeOfPomodoroData == "Week"}
         type="button"
